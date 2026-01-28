35fdeb02096157536e3a683b2774054e7cf56b76
John Corser <johnpc@umich.edu>
2026-01-28 10:10:32 -0500

    fix: add GSI query permission for getSteps lambda

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,7 @@ import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 import { storage } from "./storage/resource";
 import { getSteps } from "./function/resource";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 const backend = defineBackend({
   auth,
@@ -13,6 +14,12 @@ const backend = defineBackend({
 
 const healthKitCacheTable = backend.data.resources.tables["HealthKitCache"];
 healthKitCacheTable.grantReadData(backend.getSteps.resources.lambda);
+backend.getSteps.resources.lambda.addToRolePolicy(
+  new iam.PolicyStatement({
+    actions: ["dynamodb:Query"],
+    resources: [`${healthKitCacheTable.tableArn}/index/*`],
+  }),
+);
 backend.getSteps.addEnvironment(
   "HEALTHKITCACHE_TABLE_NAME",
   healthKitCacheTable.tableName,
